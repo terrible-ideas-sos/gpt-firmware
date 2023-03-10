cfb52545300ecb7a1b0a154b27541e0e5dfd2ee9
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2023-03-11 12:46:04 +1300

    new prompt

--- generator.py
+++ generator.py
@@ -10,7 +10,7 @@ text = openai.ChatCompletion.create(
   model="gpt-3.5-turbo",
   messages=[
         {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": "write an arduino c listing for a robot"},
+        {"role": "user", "content": "Write an arduino sketch that uses hc-sr04 ultrasonic sensor and provide the distance of the senor as output in the serial port."}
     ]
 )
 

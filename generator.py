7c1c2309067da31e5906ca468405c2615f5e4c84
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2023-03-11 12:46:01 +1300

    use dotenv

--- generator.py
+++ generator.py
@@ -1,4 +1,10 @@
 import openai
+import os
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv()
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 text = openai.ChatCompletion.create(
   model="gpt-3.5-turbo",

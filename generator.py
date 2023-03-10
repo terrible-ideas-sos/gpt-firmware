224f04f4dbee4c322978a2c390d620b37c9bdea6
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2023-03-11 11:31:56 +1300

    sample

--- generator.py
+++ generator.py
@@ -4,9 +4,7 @@ text = openai.ChatCompletion.create(
   model="gpt-3.5-turbo",
   messages=[
         {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": "Who won the world series in 2020?"},
-        {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
-        {"role": "user", "content": "Where was it played?"}
+        {"role": "user", "content": "write an arduino c listing for a robot"},
     ]
 )
 

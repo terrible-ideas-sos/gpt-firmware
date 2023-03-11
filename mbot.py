e7919b276c172ca43707c2f80895822e54991c06
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2023-03-11 19:31:05 +1300

    got smiley face working

--- mbot.py
+++ mbot.py
@@ -5,11 +5,15 @@ if __name__ == "__main__":
   manifest =   """
 Use 115200 baud rate. 
 Use Makeblock Library SDK.  
+Use Makeblock LED matrix on Port 3.
 For the main include, use #include <MeMCore.h>, instead of #include <MeOrion.h>
-Left motor attached to M1. Going forward is positive value.
-Right motor attached to M2. Going forward is negative value.
+Use MeLEDMatrix class from the #include <MeLEDMatrix.h> for LED matrix.
+Use `void drawBitmap(int8_t x, int8_t y, uint8_t Bitmap_Width, uint8_t *Bitmap)` method in MeLEDMatrix.
+Use `void clearScreen()` method in MeLEDMatrix.
+Left motor attached to `M1`. Going forward is positive value.
+Right motor attached to `M2`. Going forward is negative value.
 Use 250 as the speed.
 """
 
-  generate_firmware_and_flash(manifest, "Make wheels spin in the same direction")
+  generate_firmware_and_flash(manifest, "Make wheels spin in the same direction. Display a smiley face.")
   simple_serial_start()

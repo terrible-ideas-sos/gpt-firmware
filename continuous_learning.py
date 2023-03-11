366cb07629fe7ca4a6f2c764ff4ba2240760c26e
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2023-03-11 20:18:17 +1300

    first version of continuous learning

---
+++ continuous_learning.py
@@ -0,0 +1,39 @@
+import openai
+from generator import generate_prompt,get_code_from_openai
+from arduinointerface import flashSketch
+
+messages = [{"role": "system", "content": "You are a helpful assistant."}]
+
+manifest =   """
+Use 115200 baud rate. 
+Use Makeblock Library SDK.  
+Use Makeblock LED matrix on Port 3.
+For the main include, use #include <MeMCore.h>, instead of #include <MeOrion.h>
+Use MeLEDMatrix class from the #include <MeLEDMatrix.h> for LED matrix.
+Use `void drawBitmap(int8_t x, int8_t y, uint8_t Bitmap_Width, uint8_t *Bitmap)` method in MeLEDMatrix.
+Use `void clearScreen()` method in MeLEDMatrix.
+Left motor attached to `M1`. Going forward is positive value.
+Right motor attached to `M2`. Going forward is negative value.
+Use `void run(int speed)` method in MeDCMotor.
+Use 250 as the speed.
+"""
+
+baseline_objective = input("Set the baseline objective: ")
+messages.append({ "role": "user", "content": generate_prompt(manifest, baseline_objective) })
+
+print(messages)
+completion = openai.ChatCompletion.create(model="gpt-3.5-turbo",messages=messages)
+code = get_code_from_openai(completion)
+flashSketch(code)
+bot_message = completion["choices"][0]["message"]
+messages.append(bot_message)
+
+while True:
+    changes = input("What should be changed: ")
+    print("You entered: " + changes)
+    messages.append({ "role": "user", "content": changes })
+    completion = openai.ChatCompletion.create(model="gpt-3.5-turbo",messages=messages)
+    bot_message = completion["choices"][0]["message"]
+    messages.append(bot_message)
+
+    # Do something with the user's input here...

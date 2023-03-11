407a6976a9308e8a05a867a3643d8aa68f8105f4
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2023-03-11 15:25:50 +1300

    account for first line being arduino, c++ or c

--- generator.py
+++ generator.py
@@ -10,7 +10,13 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 def extract_code_block_from_text(text):
     start = text.find("```")
     end = text.find("```", start + 3)
-    return text[start + 3:end]
+    code = text[start + 3:end]
+    # check if first line is "arduino", "c" or "c++"
+    # case insensitve
+    if code.splitlines()[0].lower() in ["arduino", "c", "c++"]:
+        # remove first line
+        code = code.splitlines()[1:].join("\n")
+    return code
 
 def get_code_from_openai(completion):
   message = completion["choices"][0]["message"]["content"]

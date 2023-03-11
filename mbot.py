92494a3dbda8111de29ce3b3409fd4b499913a81
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2023-03-11 18:54:15 +1300

    new prompt

--- mbot.py
+++ mbot.py
@@ -6,9 +6,9 @@ if __name__ == "__main__":
 Use 115200 baud rate. 
 Use Makeblock Library SDK.  
 For the main include, use #include <MeMCore.h>, instead of #include <MeOrion.h>
-Left motor attached to port 1.
-Right motor attached to port 2.
+Left motor attached to port 1. Going forward is positive value.
+Right motor attached to port 2. Going forward is negative value.
 """
 
-  generate_firmware_and_flash(manifest, "Make wheels spin.")
+  generate_firmware_and_flash(manifest, "Make wheels spin in the same direction")
   simple_serial_start()
